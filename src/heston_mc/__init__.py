615a2ef596108ae1e7d139d6e52a347e7032d47b
Lin Tian <146679274+chiggachuuuuu@users.noreply.github.com>
2026-04-19 00:08:57 -0400

    update __init__

--- src/heston_mc/__init__.py
+++ src/heston_mc/__init__.py
@@ -4,6 +4,11 @@ from .interfaces import SimulationResult, PricingResult
 from .variance_option import variance_option_payoff, price_variance_option
 
 __all__ = [
+    "apply_control_variate",
+    "expected_average_variance",
+    "price_variance_option_control_variate",
+    "price_variance_swap_control_variate",
+    "standard_error_improvement_ratio",
     "HestonParams",
     "MonteCarloConfig",
     "default_heston_params",
@@ -13,3 +18,11 @@ __all__ = [
     "variance_option_payoff",
     "price_variance_option",
 ]
+
+from .control_variate import (
+    apply_control_variate,
+    expected_average_variance,
+    price_variance_option_control_variate,
+    price_variance_swap_control_variate,
+    standard_error_improvement_ratio,
+)
